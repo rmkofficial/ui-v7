dad19ae81687fd823acfb84373ef30e3964aedb0
rmkofficial <kmert0@outlook.com>
2024-05-28 19:46:56 +0300

    Refactor Scene component and add data grid functionality

--- src/components/Settings/Scene/Scene.jsx
+++ src/components/Settings/Scene/Scene.jsx
@@ -1,18 +1,74 @@
+import { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import SceneDropdown from "./SceneDropdown";
+import SceneDataGrid from "./SceneDataGrid";
+import setDataJson from "../../setData.json";
 import PropTypes from "prop-types";
 
-export default function Scene({ onSave, selectedRow }) {
+const Scene = ({ onSave }) => {
+  const [data, setData] = useState({ individuals: [] });
+  const [selectedIndividuals, setSelectedIndividuals] = useState([]);
+  const [rows, setRows] = useState([]);
+
+  useEffect(() => {
+    setData(setDataJson);
+    const initialRows = Array.from({ length: 32 }, (_, index) => ({
+      id: index + 1,
+      scene: `scene-${index + 1}`,
+      percentage: Math.floor(Math.random() * 101),
+    }));
+    setRows(initialRows);
+  }, []);
+
+  const handleIndividualChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setSelectedIndividuals(
+      typeof value === "string" ? value.split(",") : value
+    );
+  };
+
+  const handleSave = () => {
+    const selectedData = {
+      individuals: selectedIndividuals,
+      dataGrid: rows,
+    };
+    onSave(selectedData);
+  };
+
   return (
-    <div>
-      
-      <p>Scene Component</p>
-      <button onClick={() => onSave({ selectedRow, type: "Scene" })}>
-        Save Scene
-      </button>
-    </div>
+    <Box sx={{ display: "flex", flexDirection: "column", width: "100%" }}>
+      <Box sx={{ display: "flex", flexDirection: "row", width: "100%" }}>
+        <Box sx={{ width: "25%", paddingRight: 2 }}>
+          <SceneDropdown
+            label="Individual"
+            options={data.individuals}
+            selectedOptions={selectedIndividuals}
+            handleChange={handleIndividualChange}
+          />
+        </Box>
+        <Box sx={{ width: "75%", paddingLeft: 2 }}>
+          <SceneDataGrid rows={rows} setRows={setRows} />
+          <Box sx={{ marginTop: 2, display: "flex" }}>
+            <Button
+              type="button"
+              variant="contained"
+              color="primary"
+              onClick={handleSave}
+            >
+              SAVE
+            </Button>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
   );
-}
+};
 
 Scene.propTypes = {
   onSave: PropTypes.func.isRequired,
-  selectedRow: PropTypes.object,
 };
+
+export default Scene;
---
+++ src/components/Settings/Scene/SceneDataGrid.jsx
@@ -0,0 +1,47 @@
+import { DataGrid } from "@mui/x-data-grid";
+import PropTypes from "prop-types";
+
+const SceneDataGrid = ({ rows, setRows }) => {
+  const handleEditCellChange = (params) => {
+    const updatedRows = rows.map((row) =>
+      row.id === params.id ? { ...row, [params.field]: params.value } : row
+    );
+    setRows(updatedRows);
+  };
+
+  const columns = [
+    { field: "scene", headerName: "Scene", width: 150, editable: false },
+    {
+      field: "percentage",
+      headerName: "Percentage",
+      type: "number",
+      width: 180,
+      editable: true,
+      align: "left",
+      headerAlign: "left",
+    },
+  ];
+
+  return (
+    <div style={{ height: 600, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        onEditCellChangeCommitted={handleEditCellChange}
+      />
+    </div>
+  );
+};
+
+SceneDataGrid.propTypes = {
+  rows: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      scene: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  setRows: PropTypes.func.isRequired,
+};
+
+export default SceneDataGrid;
